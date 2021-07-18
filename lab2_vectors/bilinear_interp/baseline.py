05a9a44bd15f7e84771d885a67fe7a7d32ae1180
NCJ Charlie <me@ncj.wiki>
2021-07-18 13:56:54 +0800

    solution

--- lab2_vectors/bilinear_interp/baseline.py
+++ lab2_vectors/bilinear_interp/baseline.py
@@ -1,13 +1,13 @@
 import numpy as np
-from numpy import int32
+from numpy import int64
 
 
 def bilinear_interp_baseline(a: np.ndarray, b: np.ndarray) -> np.ndarray:
     """
     This is the baseline implementation of bilinear interpolation without vectorization.
-    - a is a ND array with shape [N, H1, W1, C], dtype = int32
+    - a is a ND array with shape [N, H1, W1, C], dtype = int64
     - b is a ND array with shape [N, H2, W2, 2], dtype = float64
-    - return a ND array with shape [N, H2, W2, C], dtype = int32
+    - return a ND array with shape [N, H2, W2, C], dtype = int64
     """
     # Get axis size from ndarray shape
     N, H1, W1, C = a.shape
@@ -15,7 +15,7 @@ def bilinear_interp_baseline(a: np.ndarray, b: np.ndarray) -> np.ndarray:
     assert N == N1
 
     # Do iteration
-    res = np.empty((N, H2, W2, C), dtype=int32)
+    res = np.empty((N, H2, W2, C), dtype=int64)
     for n in range(N):
         for i in range(H2):
             for j in range(W2):
@@ -24,5 +24,5 @@ def bilinear_interp_baseline(a: np.ndarray, b: np.ndarray) -> np.ndarray:
                 _x, _y = x - x_idx, y - y_idx
                 # For simplicity, we assume all x are in [0, H1 - 1), all y are in [0, W1 - 1)
                 res[n, i, j] = a[n, x_idx, y_idx] * (1 - _x) * (1 - _y) + a[n, x_idx + 1, y_idx] * _x * (1 - _y) + \
-                               a[n, x_idx, y_idx + 1] * (1 - _x) * (1 - _y) + a[n, x_idx, y_idx] * _x * _y
+                               a[n, x_idx, y_idx + 1] * (1 - _x) * _y + a[n, x_idx + 1, y_idx + 1] * _x * _y
     return res
--- lab2_vectors/bilinear_interp/vectorized.py
+++ lab2_vectors/bilinear_interp/vectorized.py
@@ -1,19 +1,33 @@
 import numpy as np
-from numpy import int32
+from numpy import int64
 
 
 def bilinear_interp_vectorized(a: np.ndarray, b: np.ndarray) -> np.ndarray:
     """
     This is the vectorized implementation of bilinear interpolation.
-    - a is a ND array with shape [N, H1, W1, C], dtype = int32
+    - a is a ND array with shape [N, H1, W1, C], dtype = int64
     - b is a ND array with shape [N, H2, W2, 2], dtype = float64
-    - return a ND array with shape [N, H2, W2, C], dtype = int32
+    - return a ND array with shape [N, H2, W2, C], dtype = int64
     """
     # get axis size from ndarray shape
     N, H1, W1, C = a.shape
     N1, H2, W2, _ = b.shape
     assert N == N1
 
-    res = np.empty((N, H2, W2, C), dtype=int32)
     # TODO: Implement vectorized bilinear interpolation
-    return res
+    idx_C = np.floor(b).astype(int)
+    shift = b - idx_C
+    shift_x = shift[:, :, :, 0]
+    shift_y = shift[:, :, :, 1]
+    idx_N = np.arange(N).repeat(H2 * W2)
+    idx_H1 = idx_C[:, :, :, 0]
+    idx_W1 = idx_C[:, :, :, 1]
+    a_00 = a[idx_N, idx_H1.reshape(-1), idx_W1.reshape(-1)]
+    a_10 = a[idx_N, idx_H1.reshape(-1) + 1, idx_W1.reshape(-1)]
+    a_01 = a[idx_N, idx_H1.reshape(-1), idx_W1.reshape(-1) + 1]
+    a_11 = a[idx_N, idx_H1.reshape(-1) + 1, idx_W1.reshape(-1) + 1]
+    res = a_00.reshape(-1) * ((1 - shift_x) * (1 - shift_y)).repeat(C) + \
+          a_10.reshape(-1) * (shift_x * (1 - shift_y)).repeat(C) + \
+          a_01.reshape(-1) * ((1 - shift_x) * shift_y).repeat(C) + \
+          a_11.reshape(-1) * (shift_x * shift_y).repeat(C)
+    return res.reshape(N, H2, W2, C).astype(int64)
